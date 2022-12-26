f78496ac7fddf54fedffe0b136c81e1919849c54
diwangaamasith <diwangaamasith@gmail.com>
2022-12-26 22:57:54 +0530

    row one animated

--- script.js
+++ script.js
@@ -9,30 +9,30 @@ console.log(ctx);
 const playerImage = new Image(); // image constructor
 playerImage.src = './project1/shadow_dog.png'
 //let x = 0;
-let frameX =0;
-let frameY = 9;
-let gameFrame= 0;
+let frameX = 0;
+let frameY = 3;
+let gameFrame = 0;
 const straggerFrames = 5;
 
 
-function animate(){
+function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
- //   ctx.fillRect(x,50,100,100);
-  //  ctx.drawImage(playerImage, 0, 0 ,CANVAS_WIDTH, CANVAS_HEIGHT);
-  // ctx.drawImage(playerImage,0,0)
- // ctx.drawImage(playerImage,sx,sv,sw,sh,dx,dy,dw,dh); // drawimage mthd has 3 versions
-    ctx.drawImage(playerImage,frameX*SPRITE_WIDTH,frameY*SPRITE_HEIGHT,575,523,0,0,SPRITE_WIDTH,SPRITE_HEIGHT); // drawimage mthd has 3 versions
-//    x++;
-if(gameFrame % straggerFrames == 0){
-    if(frameX<6){
-        frameX++;
-    }else{
-        frameX=0;
-    }
-}
+   let position = Math.floor(gameFrame/straggerFrames) % 6;// **
+  frameX = SPRITE_WIDTH * position;
+   ctx.drawImage(playerImage, frameX, frameY * SPRITE_HEIGHT, 575, 523, 0, 0, SPRITE_WIDTH, SPRITE_HEIGHT); // drawimage mthd has 3 versions
+
     gameFrame++;
     requestAnimationFrame(animate);// only one time
 }
 animate();
-//sprite sheet Width = 6876px /12   573 
+
+
+
+
+//sprite sheet Width = 6876px /12   573
 //sprite sheet Height = 5230px /12  523
+//   ctx.fillRect(x,50,100,100);
+  //  ctx.drawImage(playerImage, 0, 0 ,CANVAS_WIDTH, CANVAS_HEIGHT);
+  // ctx.drawImage(playerImage,0,0)
+ // ctx.drawImage(playerImage,sx,sv,sw,sh,dx,dy,dw,dh); // drawimage mthd has 3 versions
+//    x++;
